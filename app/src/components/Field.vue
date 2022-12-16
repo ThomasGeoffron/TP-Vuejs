9641c8ac5169ac468cb74969301c3ebb5f07799c
Antoine lin <lin.antoine.pro@gmail.com>
2022-12-16 17:44:17 +0100

    add field

---
+++ app/src/components/Field.vue
@@ -0,0 +1,29 @@
+<template>
+    <component
+        :is="props.as"
+        :name="name"
+        :modelValue="formikData.values[name]"
+        @update:modelValue="( value ) => formikData.updateValue(name, value)"
+    >
+        <slot/>
+    </component>
+</template>
+
+<script setup>
+import { defineProps, inject } from "vue";
+
+const formikData = inject('formik-data')
+
+const props = defineProps({
+    as: {
+        type: String | Object,
+        required: true,
+        default: 'input'
+    },
+    name: {
+        type: String,
+        required: true,
+    }
+});
+
+</script>
