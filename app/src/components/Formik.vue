3f4c3432ddac8737be6ab7fddbfc51d989607562
Arkeonn <tgeoffron@myges.fr>
2022-12-16 17:31:33 +0100

    Init Formik.vue

---
+++ app/src/components/Formik.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { provide, reactive } from 'vue';
+
+const props = defineProps({
+  initialValues: {
+    type: Object,
+    default: {},
+  },
+  validate: {
+    type: Function,
+    default: null,
+  },
+});
+
+const emit = defineEmits(["onSubmit"]);
+
+const state = reactive({
+  values: props.initialValues,
+  errors: {},
+  isSubmitting: false,
+  addValue: (name, value = "") => {
+    state.values[name] = value;
+  },
+  updateValue: (name, value) => {
+    state.values[name] = value;
+  },
+  handleSubmit: async (e) => {
+    state.isSubmitting = true;
+    state.errors = await props.validate(state.values);
+    
+    if (Object.keys(state.errors).length === 0) {
+      emit('onSubmit', state.values);
+    }
+    state.isSubmitting = false;
+  }
+});
+
+provide('formik-data', state);
+
+</script>
+
+<template>
+  <form @onSubmit.prevent="state.handleSubmit">
+    <slot 
+      :values="state.values" 
+      :errors="state.errors" 
+      :handleSubmit="state.handleSubmit" 
+      :isSubmitting="state.isSubmitting">
+    </slot>
+  </form>
+</template>
